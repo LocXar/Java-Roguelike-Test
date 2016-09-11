3d6ecc9970a9bdbd00c08c15e45a264fee79d769
LocXar <patrick.hasel@web.de>
2016-09-11 03:50:30 +0200

    add GameOver Class
    add the GameOver Class to handle the end of Gameplay by death or other
    actions.

--- src/main/java/org/locxar/core/map/Location.java
+++ src/main/java/org/locxar/core/map/Location.java
@@ -1,18 +1,46 @@
-/**
- * 
+/*
+ * Copyright (c) 2016 LocXar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.locxar.core.map;
 
+// TODO: Auto-generated Javadoc
 /**
- * @author LocXar
+ * The Class Location.
  *
+ * @author LocXar
  */
 public class Location
 {
+    
+    /** The x. */
     private int x;
+    
+    /** The y. */
     private int y;
+    
     /**
-     * 
+     * Instantiates a new location.
+     *
+     * @param x the x
+     * @param y the y
      */
     public Location(int x, int y)
     {
@@ -21,21 +49,41 @@ public class Location
 	this.y = y;
     }
     
+    /**
+     * Gets the x.
+     *
+     * @return the x
+     */
     public int getX()
     {
         return x;
     }
     
+    /**
+     * Sets the x.
+     *
+     * @param x the new x
+     */
     public void setX(int x)
     {
         this.x = x;
     }
     
+    /**
+     * Gets the y.
+     *
+     * @return the y
+     */
     public int getY()
     {
         return y;
     }
     
+    /**
+     * Sets the y.
+     *
+     * @param y the new y
+     */
     public void setY(int y)
     {
         this.y = y;
--- src/main/java/org/locxar/core/map/Mapper.java
+++ src/main/java/org/locxar/core/map/Mapper.java
@@ -1,5 +1,23 @@
-/**
- * 
+/*
+ * Copyright (c) 2016 LocXar
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.locxar.core.map;
 
@@ -9,48 +27,74 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// TODO: Auto-generated Javadoc
 /**
- * @author LocXar
+ * The Class Mapper.
  *
+ * @author LocXar
  */
 public class Mapper
 {
+    
+    /** The Constant slf4jLogger. */
     private final static Logger slf4jLogger = LoggerFactory.getLogger(Mapper.class);
-    Map<Location, String> map;
+    
+    /** The map. */
+    Map<Location, Character> map;
 
-	    /**
-	     * 
-	     */
+    /**
+     * Instantiates a new mapper.
+     */
     public Mapper()
     {
 	// TODO Auto-generated constructor stub
-	this.map = new HashMap<Location, String>();
+	this.map = new HashMap<Location, Character>();
     }
-    
-    public Mapper(Map<Location, String> map)
+
+    /**
+     * Instantiates a new mapper.
+     *
+     * @param map the map
+     */
+    public Mapper(Map<Location, Character> map)
     {
 	this.map = map;
     }
 
-    public Map<Location, String> getMap()
+    /**
+     * Gets the map.
+     *
+     * @return the map
+     */
+    public Map<Location, Character> getMap()
     {
 	return this.map;
     }
-    
-    public void setMap(Map map)
+
+    /**
+     * Sets the map.
+     *
+     * @param map the map
+     */
+    public void setMap(Map<Location, Character> map)
     {
 	this.map = map;
     }
 
-    public void addLocation(Location loc, String s)
+    /**
+     * Adds the location.
+     *
+     * @param loc the loc
+     * @param s the s
+     */
+    public void addLocation(Location loc, Character s)
     {
-	if(map.containsKey(loc))
+	if (map.containsKey(loc))
 	{
 	    this.map.put(loc, s);
-	}
-	else
+	} else
 	{
-	    slf4jLogger.error("Location found!");
+	    slf4jLogger.info("Mapper.addLocation(): Location found!");
 	}
     }
 }
